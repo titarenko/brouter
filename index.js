439cbc1ef40ba52720f88a836680ed64bf683641
Constantin Titarenko <constantin.titarenko@gmail.com>
2017-07-19 18:05:24 +0300

    alias arg mapping

--- index.js
+++ index.js
@@ -12,16 +12,18 @@ module.exports = function (...args) {
 module.exports.compose = compose
 
 const methodsMap = {
-  list: ['get', '/'],
-  view: ['get', '/:id'],
-  create: ['post', '/'],
-  update: ['put', '/:id'],
-  remove: ['delete', '/:id']
+  list: ['get', '/', (req, res) => [req.query, req, res]],
+  view: ['get', '/:id', (req, res) => [req.params.id, req, res]],
+  create: ['post', '/', (req, res) => [req.body, req, res]],
+  update: ['put', '/:id', (req, res) => [req.params.id, req.body, req, res]],
+  remove: ['delete', '/:id', (req, res) => [req.params.id, req, res]]
 }
 
 function transformRouteGroup (group) {
   return Object.keys(group).map(key => {
-    let [verb, path] = key in methodsMap ? methodsMap[key] : key.split(/\s+/)
+    let [verb, path, argsMapper = (req, res) => [req, res]] = key in methodsMap
+      ? methodsMap[key]
+      : key.split(/\s+/)
     if (!path) {
       path = verb
       verb = 'get'
@@ -37,12 +39,12 @@ function transformRouteGroup (group) {
     const middlewares = typeof group[key] === 'function'
       ? []
       : group[key].slice(0, -1)
-    return { verb, path, middlewares, handler: wrapHandler(handler) }
+    return { verb, path, middlewares, handler: wrapHandler(handler, argsMapper) }
   })
 }
 
-function wrapHandler (handler) {
-  return (req, res, next) => Promise.try(() => handler(req, res))
+function wrapHandler (handler, argsMapper) {
+  return (req, res, next) => Promise.try(() => handler(...argsMapper(req, res)))
     .then(result => {
       if (res.headersSent) {
         return
